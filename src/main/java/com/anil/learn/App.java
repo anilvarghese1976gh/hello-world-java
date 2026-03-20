97e0ea8d98599622f9fa81df93bf2b60c01ec306
Anil Varghese <anil.varghese.1976@gmail.com>
2026-03-20 12:11:08 +0530

    Add Hello World Java project with Maven build and unit tests

---
+++ src/main/java/com/anil/learn/App.java
@@ -0,0 +1,9 @@
+package com.anil.learn;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Anil!");
+        System.out.println("First Java project on GitHub!");
+        System.out.println("Learning CI/CD from scratch.");
+    }
+}
